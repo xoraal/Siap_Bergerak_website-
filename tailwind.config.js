944567f42ed87c1a7535e4f1c53232d41cfb0b3c
SA <sa@SAs-MacBook-Pro.local>
2024-11-18 08:45:53 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors :{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'blue':'20365D' ,
+      'black': '222222',
+
+    },
+   
+  },
+  
+}
